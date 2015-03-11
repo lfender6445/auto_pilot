3ab1b041bcabf99c46370dfdaa7352204393bb0f
Luke Fender <lfender6445@gmail.com>
2015-03-10 23:50:56 -0400

    more tests

--- test/test_autopilot.rb
+++ test/test_autopilot.rb
@@ -16,4 +16,22 @@ class TestAutoPilot < MiniTest::Test
     assert(File.exist? './test/stackoverflow/2015-03-10-refactoring-an-each-loop.md')
     assert(File.exist? './test/stackoverflow/2015-03-10-refactoring-an-each-loop.html')
   end
+
+  describe '#get_answers' do
+    class AutoPilot::API
+      def get_answers; [{answer_id: 123, question_id: 123}]; end
+    end
+    def setup
+      configure
+      stub_request(:get, /api.stackexchange.com/i).to_return(:status => 200, :body => {}.to_s, :headers => {})
+      stub_request(:get, "http://stackoverflow.com/questions/123/").
+        to_return(:status => 200, :body => "", :headers => {})
+    end
+    it 'returns parsed documents' do
+      @subject = AutoPilot
+      assert_kind_of(Array, @subject.get_api_answers)
+      assert_equal(@subject.get_api_answers[0].answer_id, 123)
+      assert_equal(@subject.get_api_answers[0].question_id, 123)
+    end
+  end
 end
